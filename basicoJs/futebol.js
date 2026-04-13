46adb28c5d810379c98384a3faf1702976cee38f
crianca com autismo <guilhermearodrigues09@gmail.com>
2026-04-13 15:43:40 -0300

    Create futebol.js

---
+++ basicoJs/futebol.js
@@ -0,0 +1,11 @@
+let partida1 =  2 
+let partida2 = 1 
+let partida3 = 3 
+let partida4 = 0
+let partida5 = 4 
+
+let total=(partida1+partida2+partida3+partida4+partida5)
+let media=(partida1+partida2+partida3+partida4+partida5)/5 
+
+console.log('o total de gols:',total)
+console.log('a media de gols por partida',media)
